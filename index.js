2647ce9c8e352d4a522787f2acc577ab9ce77528
benjyongzh <benj_yongzh@hotmail.com>
2023-03-15 22:06:25 +0800

    Use simpler reading of 404.

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ let http = require("http");
 let url = require("url");
 let fs = require("fs");
 
+let page404;
+fs.readFile("./404.html", (err, data) => {
+  page404 = data;
+});
+
 http
   .createServer((req, res) => {
     let q = url.parse(req.url, true);
     let filename = `.${q.pathname}`;
     if (filename === "./") {
-      //index
       fs.readFile("./index.html", (err, data) => {
         if (err) {
-          return res.end(read404(res));
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.write(page404);
+          return res.end();
         } else {
           res.writeHead(200, { "Content-Type": "text/html" });
           res.write(data);
@@ -20,20 +26,15 @@ http
     } else {
       fs.readFile(`${filename}.html`, (err, data) => {
         if (err) {
-          return res.end(read404(res));
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.write(page404);
+          return res.end();
         } else {
           res.writeHead(200, { "Content-Type": "text/html" });
           res.write(data);
+          return res.end();
         }
-        return res.end();
       });
     }
   })
   .listen(8080);
-
-function read404(res) {
-  fs.readFile("./404.html", (err, data) => {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    return data;
-  });
-}
