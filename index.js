a9d9c7798909e9cd1641fd1ecafa2847f1f5f608
Benjamin Yong <benj_yongzh@hotmail.com>
2023-03-22 22:59:13 +0800

    Add app.get. Have not solved 404.html.

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-let http = require("http");
-let url = require("url");
-let fs = require("fs");
+// let fs = require("fs");
 
-let page404;
-fs.readFile("./404.html", (err, data) => {
-  page404 = data;
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+// let page404;
+// fs.readFile("./404.html", (err, data) => {
+//   page404 = data;
+// });
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-http
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "/contact-me.html"));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(404).sendFile(path.join(__dirname, "/404.html"));
+});
+
+/* http
   .createServer((req, res) => {
     let q = url.parse(req.url, true);
     let filename = `.${q.pathname}`;
@@ -37,4 +57,8 @@ http
       });
     }
   })
-  .listen(8080);
+  .listen(8080); */
+
+app.listen(port, function () {
+  console.log(`app listening on port ${port}!`);
+});
