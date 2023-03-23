bce8a48f02f48ea7751c8fc26cb023e3a994dc23
Benjamin Yong <benj_yongzh@hotmail.com>
2023-03-23 20:13:42 +0800

    Refactor to ejs.

--- index.js
+++ index.js
@@ -1,62 +1,25 @@
-// let fs = require("fs");
-
 const express = require("express");
 const app = express();
 const port = 3000;
-const path = require("path");
 
-// let page404;
-// fs.readFile("./404.html", (err, data) => {
-//   page404 = data;
-// });
+//views
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "/about.html"));
+  res.render("about");
 });
 
 app.get("/contact-me", (req, res) => {
-  res.sendFile(path.join(__dirname, "/contact-me.html"));
+  res.render("contact-me");
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "/404.html"));
+  res.status(404).render("404");
 });
-
-/* http
-  .createServer((req, res) => {
-    let q = url.parse(req.url, true);
-    let filename = `.${q.pathname}`;
-    if (filename === "./") {
-      fs.readFile("./index.html", (err, data) => {
-        if (err) {
-          res.writeHead(404, { "Content-Type": "text/html" });
-          res.write(page404);
-          return res.end();
-        } else {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.write(data);
-          return res.end();
-        }
-      });
-    } else {
-      fs.readFile(`${filename}.html`, (err, data) => {
-        if (err) {
-          res.writeHead(404, { "Content-Type": "text/html" });
-          res.write(page404);
-          return res.end();
-        } else {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.write(data);
-          return res.end();
-        }
-      });
-    }
-  })
-  .listen(8080); */
 
 app.listen(port, function () {
   console.log(`app listening on port ${port}!`);
